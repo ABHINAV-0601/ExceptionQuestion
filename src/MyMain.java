f63b8028950eae0830e7328b867c6b74718535a2
Abhinav Pandey <pandeyabhinav2608@gmail.com>
2022-08-25 11:32:29 +0530

    completed with the finally block

--- src/MyMain.java
+++ src/MyMain.java
@@ -4,19 +4,23 @@ public class MyMain {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter first number ");
-        int a = scanner.nextInt();
+        int num1 = scanner.nextInt();
         System.out.println("Enter second number");
-        int b = scanner.nextInt();
-        int sum = a + b;
+        int num2 = scanner.nextInt();
+        int sum = num1 + num2;
         try{
             if(sum < 150) {
+
                 throw new MyFirstException("Custom Exception Occurred");
+
             }else {
                 System.out.println("sum = " + sum);
             }
         }catch (MyFirstException e){
                 System.out.println(e);
                 e.printStackTrace();
+        }finally {
+            scanner.close();
         }
     }
 }
